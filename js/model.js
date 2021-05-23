4012a8f2dfa7c2e7fc984cde53354c27b608890d
heredia lucas <heredialucasfac22@hotmail.com>
2021-05-23 18:35:15 -0300

    Seguimos con prueba de crud

--- js/model.js
+++ js/model.js
@@ -1,18 +1,45 @@
-class Model {
+export default class Model {
     constructor(){
         this.view = null;
         this.todos = [];
+        this.currentId = 1;
     }
 
     setView(view) {
         this.view = view;
     }
 
-    setTodos(){
-        this.todos;
+    getTodos(){
+       return this.todos;
+    }
+
+    findTodo(id) {
+        return this.todos.findIndex((todo) => todo.id === id);
+    }
+
+    toggleCompleted(id) {
+        const index = this.findTodo(id);
+        const todo = this.todos[index];
+        todo.completed = !todo.completed;
+        console.log(this.todos);
     }
 
     addTodo(title, description){
-        console.log(todo);
+        const todo = {
+            id: this.currentId++,
+            title,
+            description,
+            completed: false,
+        }
+
+        this.todos.push(todo);
+        console.log(this.todos);
+
+        return {...todo};
+    }
+
+    removeTodo(id) {
+        const index = this.findTodo(id);
+        this.todos.splice(index, 1);
     }
 }
